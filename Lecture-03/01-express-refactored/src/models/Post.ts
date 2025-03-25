02d39afa5cd158977aa481fa5793b118ab511973
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-03-25 11:01:10 +0100

    add date to class Post

--- Lecture-03/01-express-refactored/src/models/Post.ts
+++ Lecture-03/01-express-refactored/src/models/Post.ts
@@ -3,11 +3,13 @@ export class Post {
   title: string = '';
   content: string = '';
   author: string = '';
+  date: string = '';
 
   constructor(title: string, content: string, author: string) {
     this.id = Math.round(Math.random() * 1000);
     this.title = title;
     this.content = content;
     this.author = author;
+    this.date = new Date().toDateString();
   }
 }
